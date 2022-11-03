756c0819e887c752ca7060c1faa60abedb23c844
Mzazi25 <langat.caleb25@gmail.com>
2022-11-03 15:58:11 +0300

    Added Location data class

--- app/src/main/java/com/example/riccandmorty/domain/models/Location.kt
+++ app/src/main/java/com/example/riccandmorty/domain/models/Location.kt
@@ -4,8 +4,18 @@ package com.example.riccandmorty.domain.models
 import com.google.gson.annotations.SerializedName
 
 data class Location(
+    @SerializedName("created")
+    val created: String,
+    @SerializedName("dimension")
+    val dimension: String,
+    @SerializedName("id")
+    val id: Int,
     @SerializedName("name")
     val name: String,
+    @SerializedName("residents")
+    val residents: List<String>,
+    @SerializedName("type")
+    val type: String,
     @SerializedName("url")
     val url: String
 )
