69eb5bcb990c51e105327612acdaa6d7c3d6adc6
Mzazi25 <langat.caleb25@gmail.com>
2022-11-03 16:01:24 +0300

    Created Episodes Model Class

---
+++ app/src/main/java/com/example/riccandmorty/domain/models/Episodes.kt
@@ -0,0 +1,21 @@
+package com.example.riccandmorty.domain.models
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Episodes(
+    @SerializedName("air_date")
+    val airDate: String,
+    @SerializedName("characters")
+    val characters: List<String>,
+    @SerializedName("created")
+    val created: String,
+    @SerializedName("episode")
+    val episode: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("url")
+    val url: String
+)
