e31a4a3c6a3f9f2c69728322e697cd34ca27fa16
Mzazi25 <langat.caleb25@gmail.com>
2022-11-30 08:27:47 +0300

    Created functions for the color palette on the details view model

--- app/src/main/java/com/example/riccandmorty/presentation/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/riccandmorty/presentation/details/DetailsViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.riccandmorty.presentation.details
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.riccandmorty.domain.use_case.GetCharacterDetailsUseCase
 import com.example.riccandmorty.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,6 +18,22 @@ class DetailsViewModel @Inject constructor(
 ): ViewModel(){
     private val _state: MutableStateFlow<DetailsState> = MutableStateFlow(DetailsState())
     val state: StateFlow<DetailsState> = _state.asStateFlow()
+
+    private val _uiEvent = MutableSharedFlow<UiEvent>()
+    val uiEvent: SharedFlow<UiEvent> = _uiEvent.asSharedFlow()
+
+    private val _colorPalette: MutableState<Map<String, String>> = mutableStateOf(mapOf())
+    val colorPalette: State<Map<String, String>> = _colorPalette
+
+    fun generatePaletteColors(){
+        viewModelScope.launch {
+            _uiEvent.emit(UiEvent.GenerateColorsPalette)
+        }
+    }
+
+    fun setColorPalette(colors:Map<String, String>){
+        _colorPalette.value = colors
+    }
 
     fun onGetCharacterEvent(id:Int){
         useCase(id).onEach {result->
@@ -36,4 +56,8 @@ class DetailsViewModel @Inject constructor(
             }
         }.launchIn(viewModelScope)
     }
+}
+
+sealed class UiEvent{
+    object GenerateColorsPalette:UiEvent()
 }
