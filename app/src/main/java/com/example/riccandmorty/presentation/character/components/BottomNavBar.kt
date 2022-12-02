b3ce30aa761cabe7559790ab8684a4b79a308eef
Mzazi25 <langat.caleb25@gmail.com>
2022-12-02 23:20:22 +0300

    Updated Readme

--- app/src/main/java/com/example/riccandmorty/presentation/character/components/BottomNavBar.kt
+++ app/src/main/java/com/example/riccandmorty/presentation/character/components/BottomNavBar.kt
@@ -1,7 +1,8 @@
 package com.example.riccandmorty.presentation.character.components
 
-import androidx.compose.foundation.background
-import androidx.compose.material.*
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
+import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocationOn
@@ -9,10 +10,8 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.riccandmorty.R
 import com.example.riccandmorty.presentation.destinations.CharacterScreenDestination
