06a9de8b68e7c84b79835e3a430afdc01f919c25
Caleb Langat <95022986+Mzazi25@users.noreply.github.com>
2022-12-24 18:48:09 +0300

    WIP viewmodel tests

---
+++ app/src/test/java/com/example/riccandmorty/presentation/details/DetailsViewModelTest.kt
@@ -0,0 +1,42 @@
+package com.example.riccandmorty.presentation.details
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.riccandmorty.domain.use_case.GetCharacterDetailsUseCase
+import com.example.riccandmorty.resource.fakeCharacterDto
+import com.example.riccandmorty.util.Resource
+import com.google.common.truth.Truth
+import io.mockk.mockkClass
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.runBlocking
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.`when`
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+class DetailsViewModelTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var viewModel: DetailsViewModel
+    private var getCharacterDetailsUseCase:GetCharacterDetailsUseCase = mockkClass(GetCharacterDetailsUseCase::class)
+
+
+    // WIP
+
+    @Test
+    fun `get character details, should return Error`()= runBlocking{
+        `when`(getCharacterDetailsUseCase.invoke(2)).thenReturn(
+            flow {
+                emit(Resource.Success(fakeCharacterDto))
+            }
+        )
+        viewModel = DetailsViewModel(getCharacterDetailsUseCase)
+        viewModel.onGetCharacterEvent(2)
+
+        val result = viewModel.onGetCharacterEvent(2)
+        Truth.assertThat(result).isSameInstanceAs(fakeCharacterDto)
+    }
+
+
+}
